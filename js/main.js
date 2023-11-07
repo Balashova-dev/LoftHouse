5350f09b81fc6b9e1325d2f7dc8b18acba0a7b45
NeuralNat <ifrance@yandex.ru>
2023-11-07 18:25:55 +0300

    JS mobile nav toggle

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.header__top-row')
 
-console.log(navIcon);
 navBtn.onclick = function () {
     navIcon.classList.toggle('nav-icon--active');
+    nav.classList.toggle('header__top-row--mobile');
 };
