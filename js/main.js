20ffda1ed720dba384faeff7c9ea1498b75d8aac
NeuralNat <ifrance@yandex.ru>
2023-11-11 22:02:56 +0300

    delete '+'  and show  placeholder if  filed is empty

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ navBtn.onclick = function () {
 /* Phone Mask */
 
 mask('[data-tel-input]');
+
+// Удаляем '+', если больше ничего не введено, чтобы показать Placeholder 
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', ()=> {
+    if (input.value == '+') input.value = '';
+    })
+    input.addEventListener('blur', ()=> {
+    if (input.value == '+') input.value = '';
+    })
+});
